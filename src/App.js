d7450a04a0913a598822d072115ee734106147e5
Tyler Lai <lai.t.tyler@gmail.com>
2020-12-26 14:56:38 -0600

    Update maxlength input

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
                 onChange={this.handleChange}
                 value={this.state.username}
                 minLength="3"
-                maxLength="4"
+                maxLength="20"
                 required
               />
               <label htmlFor="item">Item</label>
